2ae8ee8f63c1cc17405836f3e055377b2d6dce15
Tom Barnes <ruralmyth@gmail.com>
2016-01-25 18:27:02 +0000

    move todo list to controller and use ng-repeat

--- do_stuff/js/doStuffController.js
+++ do_stuff/js/doStuffController.js
@@ -1,3 +1,25 @@
 doStuff.controller('doStuffController', [function() {
+  this.toDoList = [
+    {
+      name: 'Build ToDo app',
+      isCompleted: false
+    },
+    {
+      name: 'Hang out washing',
+      isCompleted: false
+    },
+    {
+      name: 'more stuff...',
+      isCompleted: false
+  }];
 
+  this.sumActive = function(){
+    var active = 0;  
+    this.toDoList.forEach(function(task){
+      if (task.isCompleted===false) {
+        active++
+      }     
+    });
+    return active;
+  };
 }]);
